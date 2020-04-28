021de63e0e49717adfa2347e3259de086b28fc86
pablitocoder <35904158+pablitocoder@users.noreply.github.com>
2020-04-29 01:00:00 +0200

    adding api-connection to Vue

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,9 @@
                         <div class="input-group-prepend">
                             <i class="fas fa-search fa-search-box"></i>
                         </div>
-                        <input type="text" class="form-control search-city-input" placeholder="Wpisz nazwę miejscowości..." aria-label="Username" aria-describedby="basic-addon1">
-                    </div>
+                        <input type="search" id="address-input" class="address-input" placeholder="Wpisz nazwę miejscowości..." aria-label="Username" aria-describedby="basic-addon1">
+						<button id = "app" v-on:click="getLocation">Dane dla Twojej lokacji</button>
+					</div>
                 </div>
                 <div class="col-lg-6">
                     <div class="row">
@@ -153,8 +154,107 @@
 
 export default {
   name: 'App',
-  data () {
-    return '';
-  }
+  data: {
+	placesAutocomplete : places({
+		appId: 'plZPOBCP5Q3T',
+		apiKey: 'c1f1ec07ed62d4a064179ffe3bed383a',
+		container: document.querySelector('#address-input'),
+		countries: ['pl'],
+		type: ['city']
+	  }),
+	pollut : {}
+},
+created: function() {
+	this.load_data({
+					'city':'Warszawa',
+					'country':'Polska',
+					'latlng': {'lat': 52.2370,'lng':21.0175}
+	});
+	 this.placesAutocomplete.on('change', e => this.load_data(e.suggestion));
+},
+methods: {
+	/*function loading wather and air pollution data*/
+	load_data: function(loc_data) {
+
+		let place = {"city": loc_data.city,
+					"country": loc_data.country}
+		console.log(place)
+		/* loading pollution data in json */
+		let km = -1;
+		
+		url = `https://airapi.airly.eu/v2/measurements/point?lat=${loc_data.latlng.lat}&lng=${loc_data.latlng.lng}`;
+		url1 = `https://airapi.airly.eu/v2/installations/nearest?lat=${loc_data.latlng.lat}&lng=${loc_data.latlng.lng}&maxDistanceKM=-1.0`;
+		
+		var currXHR = new XMLHttpRequest();
+		currXHR.onreadystatechange = function() {
+			if (currXHR.readyState == 4 && currXHR.status == 200) {
+				var json_data = JSON.parse(currXHR.responseText);
+				console.log(json_data)
+				var pollution = []
+				for (let i =2; i <24;i+=3){
+					let row = json_data.forecast[i]
+					pollution.push({
+									"date": row.fromDateTime.slice(0,10) + " " + row.fromDateTime.slice(11,19),
+									"PM25":row.values[0].value,
+									"PM10": row.values[1].value,
+									"PM25%": row.standards[0].percent,
+									"PM10%": row.standards[1].percent
+					})
+				} 
+				console.log(pollution)
+				this.pollut = pollution;
+			}
+		};
+
+		currXHR.open("GET", url);
+		currXHR.setRequestHeader('Accept', 'application/json');
+		currXHR.setRequestHeader('apikey', 'ALA45rSZm5sGrZ9I4ibJUq1U21O82AKR');
+		currXHR.send();
+		
+		/* loading weather data in json */
+		var forecastXHR = new XMLHttpRequest();
+		forecastXHR.onreadystatechange = function(){
+			if (forecastXHR.readyState == 4 && forecastXHR.status == 200){
+				var json_data = JSON.parse(forecastXHR.responseText);
+				let city = json_data.city.name
+				var output = []
+				for (let i = 0; i < 8; i++){
+					var row = json_data.list[i]
+					var rain_data = 0
+					if (typeof row.main.rain != "undefined"){
+						rain_data = row.main.rain("3h")
+					};
+					output.push({ "date" : row.dt_txt,
+											"temperature": Math.round(row.main.temp - 273),
+											"pressure": row.main.pressure,
+											"wind": row.wind.speed,
+											"rain": rain_data,
+					})
+				}
+				console.log(output)
+			} 
+		}
+
+		forecastXHR.open('GET', `http://api.openweathermap.org/data/2.5/forecast?lat=${loc_data.latlng.lat}&lon=${loc_data.latlng.lng}&appid=96e1f29b74494a9d9469860a9ff96f14`);
+		forecastXHR.send();
+	},
+	
+	/* function checking geolocation and runing getCoords */
+	getLocation: function() {
+	  if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(this.getCoords);
+	  } else {
+		console.log("Geolocation is not supported by this browser.");
+	  }
+	  this.placesAutocomplete.setVal('')
+	},
+	/*function retreiving coordinates for geolocation and running data load*/
+	getCoords : function(position){
+		var data_arg = {"latlng": {"lat": position.coords.latitude,
+									"lng": position.coords.longitude}
+							}
+		this.load_data(data_arg)
+	}
+}
 }
 </script>
